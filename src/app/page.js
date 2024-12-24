961fa3f642852608640bfafe60ffdb51e471c4b6
Sparshgarg14 <ssparsh2000@gmail.com>
2024-12-24 16:42:14 +0530

    leetcode changes commited

--- src/app/page.js
+++ src/app/page.js
@@ -180,13 +180,13 @@ export default function Home() {
       </p>
     </div>
 
-   {/* Second Codeforces Rating */}
+   {/* Second Leetcode Rating */}
    <div className="flex flex-col items-center">
       <div className="h-40 w-40 flex items-center justify-center bg-gradient-to-r from-orange-400 to-orange-600 rounded-full shadow-lg">
         <LeetcodeStats className="text-3xl text-white" handle="SG_14" />
       </div>
       <p className="text-lg text-gray-800 dark:text-gray-300 text-center max-w-sm mt-4">
-        Another competitive programming journey showcased through Codeforces.
+        Another competitive programming journey showcased through Leetcode.
       </p>
     </div>
   </div>
